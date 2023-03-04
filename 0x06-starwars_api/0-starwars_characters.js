4c313a4ceeeaeabb9ee111bec37db0fe423e478a
Selam25-30 <selamawitbirhanu123@g,mail.com>
2023-03-04 00:56:01 -0800

    nqueens

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* prints all characters of star wars movie */
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,3 @@
+# 0-starwars_characters.js
+
+0x06. Star Wars API
