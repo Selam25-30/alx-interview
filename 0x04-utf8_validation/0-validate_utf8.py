6b33c63a3654efa292be6f6b55a266b00c032a31
Selam25-30 <selamawitbirhanu123@g,mail.com>
2023-03-04 00:54:32 -0800

    validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+A module that contains a function that determines if a given data set
+represents a valid UTF-8 encoding.
+'''
+
+
+def validUTF8(data):
+    '''
+    A function that determines if a given data set is a valid
+    UTF-8 encoding
+    '''
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
